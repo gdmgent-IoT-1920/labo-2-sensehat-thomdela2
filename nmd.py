385ef062a1d8d445f40d498dfcda58441a313f0f
thomdela2 <thomdela2@student.arteveldehs.be>
2020-02-26 10:29:58 +0100

    nmd.py initialized

---
+++ nmd.py
@@ -0,0 +1,4 @@
+# @TODO Visualiseer iedere letter, in random kleur, uit de string NMD via methode show_letter en een loop (1s per iteratie). Na laatste letter herhaalt voorgaande functionaliteit na 3s
+# Uitbreiding: Maak de tekst (string) en de snelheid van de loop instelbaar via de input van de CLI.
+
+import sense_hat from SenseHat 
