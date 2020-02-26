b32f97c7e15c3b48c983a75ad1d310bc2ea6dbc2
thomdela2 <thomdela2@student.arteveldehs.be>
2020-02-26 11:32:25 +0100

    baseline.py finished

--- baseline.py
+++ baseline.py
@@ -1,1 +1,10 @@
 from sense_hat import SenseHat 
+from time import sleep
+from random import randint
+
+sense = SenseHat()
+
+random_colour1 = (randint(0, 255), randint(0, 255), randint(0, 255)) 
+random_colour2 = (randint(0, 255), randint(0, 255), randint(0, 255)) 
+
+sense.show_message("Hello! We are New Media Development :)",text_colour=random_colour1,back_colour=random_colour2)
