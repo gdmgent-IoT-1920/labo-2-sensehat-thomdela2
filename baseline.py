19fa6ca54415eba2565d7c362665642e4a942c9d
thomdela2 <thomdela2@student.arteveldehs.be>
2020-02-26 14:55:18 +0100

    Mario made, not finished

--- baseline.py
+++ baseline.py
@@ -5,6 +5,6 @@ from random import randint
 sense = SenseHat()
 
 random_colour1 = (randint(0, 255), randint(0, 255), randint(0, 255)) 
-random_colour2 = (randint(0, 255), randint(0, 255), randint(0, 255)) 
+random_colour2 = (randint(0, 255), randint(0, 255), randint(0, 255), 0.6) 
 
 sense.show_message("Hello! We are New Media Development :)",text_colour=random_colour1,back_colour=random_colour2)
---
+++ mario.py
@@ -0,0 +1,23 @@
+from sense_hat import SenseHat 
+from time import sleep
+from random import randint
+
+sense = SenseHat()
+
+b = (51, 102, 255) # Blauw
+a = (49, 205, 202) # Azuurblauw
+z = (0, 0, 0) # Zwart
+s = (252, 204, 156) # skin
+
+user = [
+	z,z,b,b,a,b,z,z,
+	z,b,b,s,a,s,b,z,
+	z,b,s,z,s,z,s,z,
+	z,a,s,s,s,s,s,z,
+	b,b,a,z,z,s,a,b,
+	b,z,b,a,a,b,z,b,
+	z,z,a,b,b,a,z,z,
+	z,b,b,z,z,b,b,z
+]
+
+sense.set_pixels(user)
