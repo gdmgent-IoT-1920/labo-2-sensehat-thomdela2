32369ca0a5122a069b0c06f8e88985512dd4df39
thomdela2 <thomdela2@student.arteveldehs.be>
2020-03-02 15:52:22 +0100

    Mario jumps!

--- mario.py
+++ mario.py
@@ -20,4 +20,22 @@ user = [
 	z,b,b,z,z,b,b,z
 ]
 
+jump = [
+	z,b,b,s,a,s,b,z,
+	z,b,s,z,s,z,s,z,
+	z,a,s,s,s,s,s,z,
+	b,b,a,z,z,s,a,b,
+	b,z,b,a,a,b,z,b,
+	z,z,a,b,b,a,z,z,
+	z,b,b,z,z,b,b,z,
+	z,z,z,z,z,z,z,z
+]
+
 sense.set_pixels(user)
+
+while True:
+  for event in sense.stick.get_events():
+	if event.direction == 'up':
+		sense.set_pixels(jump)
+		sleep(0.1)
+		sense.set_pixels(user)
